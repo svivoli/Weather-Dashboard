52ebac6b195bca59424fb7a4c67646cee2a9cb96
Sarah Vivoli <snvivoli@gmail.com>
2019-11-28 22:27:40 -0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # Weather-Dashboard
+
+### Description
+
+The purpose of this project was to create a weather dashboard that pulls data from OpenWeather to produce current weather stats as well as a five day forecast for a city inputted by the user. The user's search history is stored and displayed on the page so long as they stay on the page. This serves to deliver concise weather data to users.
+
+Components:
+1. Bootstrap grid
+2. CSS Styling
+3. Bootstrap navbar
+4. Search bar with input field and search button
+ - Fontawesome icon for search button
+5. Separate divs for displaying current weather and 5-day forecast
+6. Ajax calls using OpenWeather query urls and API key
+ - data indexing
+7. jQuery generated titles, text, and bootstrap cards
+ - css styling overrides bootstrap styling of cards
+8. localStorage
+9. Media queries to optimize user experience on mobile devices
+10. Googlefonts
+11. Super cool background
+
+### User Experience
+
+![Blank Page](https://i.imgur.com/KmGt2uD.jpg)
+
+The user is greeted with a somewhat barren page, their attention direction toward the search bar in the top left corner.
+
+![Current Weather](https://i.imgur.com/lxIWrKG.png)
+
+Upon inputting a city, the current weather is displayed to the right. The current weather data that is pulled includes temperature, humidity, and wind speed. The city name, date, and current weather icon are displayed at the top.
+
+![Forecast](https://i.imgur.com/t4G58Bg.png)
+
+Below the current weather, the 5-day forecast for that city is displayed. For each day, the date, weather icon, temperature, and humidity are displayed, corresponding to data at the 03:00:00 hour of that date. 
+
+![Full Page](https://i.imgur.com/jtkw9sP.jpg)
+
+As the user performs multiple searches, their search history is displayed below the search bar. 
+
+### Developer Experience
+
+Code Summary:
+- Concise
+- One for loop used for 5-day forecast
+
+Unresolved:
+- Search history lines cannot be clicked to display data for those cities
+- Media queries are a bit messy
+
+I enjoyed incorporating ajax calls into this project. Giving the user access to third-party data makes a site feel vastly more functional. I am excited to see what's possible when using more than one API.
+
+--Sarah Vivoli
+
+
--- script.js
+++ script.js
@@ -21,7 +21,6 @@ $("#citySearch").on("click", function (event) {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
 
         var $cityTitle = $("<h3>");
         $cityTitle.attr("style", "padding-bottom: 15px; font-weight: bold");
@@ -45,9 +44,6 @@ $("#citySearch").on("click", function (event) {
         $windTitle.html("Wind Speed: " + response.wind.speed + " MPH");
         cityStatsEl.append($windTitle);
 
-        // var $uvTitle = $("<h5>");
-        // $uvTitle.html("UV Index: " + response.)
-
         searchedEl = $(".searched");
 
         localStorage.setItem("citySearched", city);
@@ -67,95 +63,25 @@ $("#citySearch").on("click", function (event) {
         url: queryURL2,
         method: "GET"
     }).then(function (response2) {
-        console.log('res2', response2);
-
-        var day1El = $(".day1");
-        var day2El = $(".day2");
-        var day3El = $(".day3");
-        var day4El = $(".day4");
-        var day5El = $(".day5");
-        var dayEls = [day1El, day2El, day3El, day4El, day5El];
-
-        // var noonDates = [];
-        // for (var m = 0; m < response2.list.length; m++) {
-        //     if (response2.list[m].dt_txt.includes("12:00:00")) {
-        //         noonDates.push(response2.list[m])
-        //     }
-        // }
-        // console.log('day', noonDates)
-
-        //     for (var j = 0; j < response2.list.length; j++) {
-        //         if (response2.list[j].dt_txt.indexOf("15:00:00") !== -1) {
-        //             var $dayDiv = $("<div>");
-        //             $dayDiv.attr("style", "color: white");
-    
-        //             var $date = $("<h5>");
-        //             $date.html(mm + '/' + (dd++ + 1) + '/' + yyyy);
-        //             $date.attr("style", "padding-top: 5px; text-size: 24px; font-weight: bold");
-    
-        //             var $weatherIcon = $("<img>");
-        //             $weatherIcon.attr("src", "http://openweathermap.org/img/w/" + response2.list[j].weather[0].icon + ".png");
-    
-        //             var $dayTemp = $("<p>");
-        //             $dayTemp.html("Temp: " + response2.list[j].main.temp + " &deg;F");
-    
-        //             var $dayHum = $("<p>");
-        //             $dayHum.html("Humidity: " + response2.list[j].main.humidity + "%");
-    
-        //             $dayDiv.append($date);
-        //             $dayDiv.append($weatherIcon);
-        //             $dayDiv.append($dayTemp);
-        //             $dayDiv.append($dayHum);
-                    
-        //             dayEls[j].html($dayDiv);
-        //         }
-        //     }
-
-        // for (var j=0; j<dayEls.length; j++) {
-        //     var $dayDiv = $("<div>");
-        //     $dayDiv.attr("style", "color: white");
-        //     var $date = $("<h5>");
-        //     $date.html(mm + '/' + (dd ++ + 1) + '/' + yyyy);
-        //     $date.attr("style", "padding-top: 5px; text-size: 24px; font-weight: bold");
-        //     $dayDiv.append($date);
-        //     var $weatherIcon = $("<img>");
-        //     $weatherIcon.attr("src", "http://openweathermap.org/img/w/" + response2.list[j+4].weather[0].icon + ".png");
-        //     $dayDiv.append($weatherIcon);
-        //     var $dayTemp = $("<p>");
-        //     $dayTemp.html("Temp: " + response2.list[j+8].main.temp + " &deg;F");
-        //     $dayDiv.append($dayTemp);
-        //     var $dayHum = $("<p>");
-        //     $dayHum.html("Humidity: " + response2.list[j+8].main.humidity + "%");
-        //     $dayDiv.append($dayHum);
-        //     dayEls[j].html($dayDiv);
-        // }
 
         $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
-        // loop over all forecasts (by 3-hour increments)
         for (var i = 0; i < response2.list.length; i++) {
-          // only look at forecasts around 3:00pm
           if (response2.list[i].dt_txt.indexOf("15:00:00") !== -1) {
-            // create html elements for a bootstrap card
             var col = $("<div>").addClass("col-md-2");
             col.attr("style", "margin: 10px")
             var card = $("<div>").addClass("text-white forecastDay");
             var body = $("<div>").addClass("card-body p-2");
-            // body.attr("style", "text-align: center");
             var title = $("<h5>").addClass("card-title").text(new Date(response2.list[i].dt_txt).toLocaleDateString());
             var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response2.list[i].weather[0].icon + ".png");
             var p1 = $("<p>").addClass("card-text").text("Temp: " + response2.list[i].main.temp_max + " °F");
             p1.attr("style", "font-size: 14px")
             var p2 = $("<p>").addClass("card-text").text("Humidity: " + response2.list[i].main.humidity + "%");
             p2.attr("style", "font-size: 14px")
-            // merge together and put on page
             col.append(card.append(body.append(title, img, p1, p2)));
             $("#forecast .row").append(col);
           }
         }
-
         
-
     });
-
 
 });
